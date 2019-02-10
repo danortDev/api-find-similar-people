9987e8505a0f0e4e3316ca550f11b341f44128ab
Daniel Ortiz Lira <d.ortiz.lira@gmail.com>
2019-02-10 02:16:35 -0500

    remove db config import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'babel-polyfill';
 import express from 'express';
 import dotenv from 'dotenv';
-import initDB from './db/index.js';
+// import initDB from './db/index.js';
 import candidateRoutes from './routes/candidate';
 import peopleRoutes from './routes/people';
 

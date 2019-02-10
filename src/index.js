4ebcdb91aafbc27cb62ed4ef68be07ec5db7828d
Daniel Ortiz Lira <d.ortiz.lira@gmail.com>
2019-02-10 01:29:10 -0500

    add cors

--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ initDB();
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/candidate', candidateRoutes);
 app.use('/people', peopleRoutes);
 

e0f76aae1f3b1341ab69f5afc0162212be2fb82d
Daniel Ortiz Lira <d.ortiz.lira@gmail.com>
2019-02-09 19:33:52 -0500

    use buildCandidate in getCandidate

--- src/client/getCandidate.js
+++ src/client/getCandidate.js
@@ -1,10 +1,11 @@
 import getBios from './getBios';
 import getConnections from './getConnections';
+import { buildCandidate } from './utils';
 
 const getCandidate = async (username) => {
   const { data: bios } = await getBios(username);
   const { data: connections } = await getConnections(username);
-  return { ...bios, ...connections };
+  return buildCandidate(bios, connections);
 };
 
 export default getCandidate;
